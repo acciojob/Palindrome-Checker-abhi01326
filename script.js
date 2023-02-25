3483a2c76fb07be57f9c852bece435ee5ae899f8
abhi01326 <110410862+abhi01326@users.noreply.github.com>
2023-02-25 17:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	const len=str.lenght;
+	for(let i=0;i<len/2;i++){
+		if str[i]!=str[len-1-i]{
+			alert('false')
+		}
+	}
+	alert('true')
 }
 module.exports = palindrome
